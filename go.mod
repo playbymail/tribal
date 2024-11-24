bcd365e0c99c88b374b61f05f3f1a42de84a0a55
Michael Henderson <mdhender@mdhender.com>
2024-11-23 19:38:54 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/playbymail/tribal
+
+go 1.23.3
---
+++ version.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2024 Michael D Henderson. All rights reserved.
+
+package main
+
+import (
+	"github.com/mdhender/semver"
+)
+
+var (
+	version = semver.Version{Major: 0, Minor: 1, Patch: 0}
+)
